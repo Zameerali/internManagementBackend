5ddf049e570bc3de4b7947468f7c59726f2758b3
Zameerali <zameeralimustafa@gmail.com>
2025-07-17 05:21:55 -0700

    added the project refrence in tasks to change the relations

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,11 +2,15 @@ const db = require('../config/db.config.js');
 
 
 module.exports = {
-    addTask: async ({intern_id,title,description,task_date})=>{
-        const[result] = await db.query('INSERT into tasks (intern_id, title, description, task_date) VALUES (?, ?, ?, ?)',[intern_id,title,description,task_date])
+    addTask: async ({intern_id,project_id,title,description,task_date})=>{
+        const[result] = await db.query('INSERT into tasks (intern_id,project_id, title, description, task_date) VALUES (?, ?, ?, ?, ?)',[intern_id,project_id,title,description,task_date])
     },
     getTasksByIntern: async (intern_id) => {
         const [rows] = await db.query('SELECT * FROM tasks where intern_id = ?',[intern_id])
+        return rows
+    },
+    getTasksByProject: async (project_id) => {
+        const [rows] = await db.query('SELECT * FROM tasks where project_id = ?',[project_id])
         return rows
     },
     updateTaskStatus: async ({id, status})=>{
